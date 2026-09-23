f2cdcb4be8a48115c658d90fadb01e3b10673f94
Alvaro <hernandezsanchezalvarojose@gmail.com>
2026-09-23 20:47:00 +0200

    Introduccion de logs

--- app/src/main/java/com/example/lab1/MainActivity.kt
+++ app/src/main/java/com/example/lab1/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.lab1
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
@@ -15,6 +16,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d("Inicio", "Se esta creando la comunidad del anillo en Rivendel")
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -32,6 +34,28 @@ class MainActivity : AppCompatActivity() {
             numMiembros= numMiembros + 1
             textoMiembros.text = "Miembros reunidos: " + numMiembros
 
+            Log.d("Miembro añadido","Un nuevo miembro ha sido añadido a la comunidad del anillo")
+
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("Comienzo","La aplicacion se esta iniciando para forjar la comunidad del anillo")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("Avance del viaje","El viaje continua sin contratiempos ni baja ")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("Pausa","El ojo de Sauron nos permite tomarnos un descanso")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("Final","El anillo ha sido destruido, la mision ha sido completada")
     }
 }
